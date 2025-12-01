e9f5c54fe136759ee696e68b21d1bbc1d8bbde18
Aaditya Saxena <aadityasaxena76@gmail.com>
2025-12-02 00:00:55 +0530

    Enhance Coin Flip Game with Pi Estimation
    Added functionality to estimate Pi using Monte Carlo method and improved the coin flip game with a scorecard.

--- Coin_Flip_Game.c
+++ Coin_Flip_Game.c
@@ -1,48 +1,85 @@
+//CoinFlipGame with Estimating value of PI using coin flips.
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+int wins = 0;
+int totalGames = 0;
+void coinFlipGame() {
+    int guess, flip;
+    printf("\n--- Coin Flip Game ---\n");
+    printf("Guess the coin: 0 = Heads, 1 = Tails: ");
+    scanf("%d", &guess);
+    if (guess != 0 && guess != 1) {
+        printf("Invalid choice! Choose 0 or 1.\n");
+        return;
+    }
+    flip = rand() % 2;
+    printf("Coin flipped: %s\n", flip == 0 ? "Heads" : "Tails");
+    if (guess == flip) {
+        printf("You guessed it right!\n");
+        wins++;
+    } else {
+        printf("Wrong guess. Try again!\n");
+    }
+    totalGames++;
+}
+void estimatePi() {
+    int points;
+    int insideCircle = 0;
+    double x, y;
+    printf("\n--- Estimate Pi ---\n");
+    printf("Enter number of random points: ");
+    scanf("%d", &points);
+    if (points <= 0) {
+        printf("Number of points must be positive!\n");
+        return;
+    }
+    for (int i = 0; i < points; i++) {
+        x = (double)rand() / RAND_MAX;
+        y = (double)rand() / RAND_MAX;
+        if (x * x + y * y <= 1) {
+            insideCircle++;
+        }
+    }
+    double pi = 4.0 * insideCircle / points;
+    printf("Estimated Pi using %d points: %.6lf\n", points, pi);
+}
+void showScorecard() {
+    printf("\n--- Scorecard ---\n");
+    printf("Total games played: %d\n", totalGames);
+    printf("Total correct guesses: %d\n", wins);
+    if (totalGames > 0) {
+        double percent = (double)wins / totalGames * 100;
+        printf("Winning percentage: %.2lf%%\n", percent);
+    }
+}
 int main() {
-    int choice, flip, score = 0, rounds = 0;
-    char playAgain;
-
-    //random number choose karne ke liye jaise python mein karte the 
+    int choice;
     srand(time(0));
-
-    printf("=== Welcome to the Coin Flip Game ===\n");
-
     do {
-        printf("\nChoose:\n1. Heads\n2. Tails\nEnter your choice: ");
+        printf("\n=== MENU ===\n");
+        printf("1. Play Coin Flip Game\n");
+        printf("2. Estimate Pi using Monte Carlo\n");
+        printf("3. Show Scorecard\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
         scanf("%d", &choice);
-
-        // Agar 1 aaya then it's Heads agar 2 aaya then it's Tails)
-        flip = (rand() % 2) + 1;
-
-        printf("Coin is flipping...\n");
-        if (flip == 1) {
-            printf("Result: Heads\n");
-        } else {
-            printf("Result: Tails\n");
+        switch (choice) {
+            case 1:
+                coinFlipGame();
+                break;
+            case 2:
+                estimatePi();
+                break;
+            case 3:
+                showScorecard();
+                break;
+            case 4:
+                printf("Exiting program. Bye!\n");
+                break;
+            default:
+                printf("Invalid choice! Try again.\n");
         }
-
-        if (choice == flip) {
-            printf("Yay...You Win!\n");
-            score++;
-        } else {
-            printf("Sorry...You Lose!\n");
-        }
-
-        rounds++;
-
-        printf("Score: %d/%d\n", score, rounds);
-
-        //Next round ke liye input 
-        printf("Do you want to play again? (y/n): ");
-        scanf(" %c", &playAgain);
-
-    } while (playAgain == 'y' || playAgain == 'Y');
-
-    printf("\nThanks for playing! Final Score: %d/%d\n", score, rounds);
-
+    } while (choice != 4);
     return 0;
 }
